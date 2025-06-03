98cf7d35de8690ad4a6f74136b787d0b57dfebef
Андрей Коваль <parabellum.koval@gmail.com>
2025-06-03 12:02:15 +0300

    text/javascript

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,22 +42,22 @@ export default defineNuxtConfig({
       },
 
       script: [
-        {
-          type: 'text/partytown',
-          innerHTML: `(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "r9p9918va4");`
-        },
-        {
-          type: 'text/partytown',
-          src: '//script.crazyegg.com/pages/scripts/0129/2992.js',
-          async: true
-        },
+        // {
+        //   type: 'text/partytown',
+        //   innerHTML: `(function(c,l,a,r,i,t,y){
+        //     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        //     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        //     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        //   })(window, document, "clarity", "script", "r9p9918va4");`
+        // },
+        // {
+        //   type: 'text/partytown',
+        //   src: '//script.crazyegg.com/pages/scripts/0129/2992.js',
+        //   async: true
+        // },
 
         {
-          type: 'text/partytown',
+          type: 'text/javascript',
           innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
