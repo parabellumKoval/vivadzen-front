39539a86469ad2929b39911b65296938a919cd70
Андрей Коваль <parabellum.koval@gmail.com>
2025-07-12 18:24:23 +0300

    ecom events

--- composables/useGoogleEvent.ts
+++ composables/useGoogleEvent.ts
@@ -251,8 +251,11 @@ export const useGoogleEvent = () => {
     const handler = eventHandlers[functionName as keyof EventHandlers];
 
     const gTagData = handler(data)
-    console.log('event', gTagData.event, {ecommerce: gTagData.ecommerce})
-    gtag('event', gTagData.event, {ecommerce: gTagData.ecommerce})
+    // console.log('event', gTagData.event, gTagData.ecommerce)
+    gtag('event', gTagData.event, {
+      ...gTagData.ecommerce,
+      debug_mode: true
+    })
   }
 
   return {
