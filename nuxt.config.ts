4043dccba78e902e050dfb715fc464fa505796cb
Андрей Коваль <parabellum.koval@gmail.com>
2025-07-23 13:38:34 +0300

    consent granted

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -229,6 +229,21 @@ export default defineNuxtConfig({
 
   gtag: {
     id: process.env.GTAG,
+    initCommands: [
+      // Setup up consent mode
+      ['consent', 'default', {
+        // ad_user_data: 'denied',
+        // ad_personalization: 'denied',
+        // ad_storage: 'denied',
+        // analytics_storage: 'denied',
+        // wait_for_update: 500,
+        ad_user_data: 'granted',
+        ad_personalization: 'granted',
+        ad_storage: 'granted',
+        analytics_storage: 'granted',
+        // wait_for_update: 500,
+      }]
+    ]
   },
   
   i18n: {
