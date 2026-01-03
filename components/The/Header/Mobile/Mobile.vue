962e8770b6e1ff822a064aa5b0843a894cab59ef
Андрей Коваль <parabellum.koval@gmail.com>
2026-01-03 13:28:19 +0200

    </ClientOnly>

--- components/The/Header/Mobile/Mobile.vue
+++ components/The/Header/Mobile/Mobile.vue
@@ -7,24 +7,25 @@ const { isVisible } = useHeaderScroll()
 <template>
   <header class="header" :class="{ 'header--hidden': !isVisible}">
     <div class="header-container full-container">
+      <ClientOnly>
+        <!-- NOTIFICATIONS -->
+        <the-header-notifications class="notifications"></the-header-notifications>
 
-      <!-- NOTIFICATIONS -->
-      <the-header-notifications class="notifications"></the-header-notifications>
+        <!-- LOGO -->
+        <NuxtLink :to="$regionPath('/')" class="logo" clickable>
+          <nuxt-img
+            src = "/images/company.png"
+            width = "200"
+            height = "58"
+            sizes = "mobile:200px tablet:200px desktop:220px"
+            fit = "inside"
+            format = "avif"
+            class = "logo-img"
+          />
+        </NuxtLink>
 
-      <!-- LOGO -->
-      <NuxtLink :to="$regionPath('/')" class="logo" clickable>
-        <nuxt-img
-          src = "/images/company.png"
-          width = "200"
-          height = "58"
-          sizes = "mobile:200px tablet:200px desktop:220px"
-          fit = "inside"
-          format = "avif"
-          class = "logo-img"
-        />
-      </NuxtLink>
-
-      <the-header-region class="region"></the-header-region>
+        <the-header-region class="region"></the-header-region>
+      </ClientOnly>
     </div>
   </header>
 </template>
